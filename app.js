01285e948716784332e98baaca92d9ac06d19f06
Patern14 <paternmalle@gmail.com>
2021-05-10 02:43:30 +0100

    Answers and arrows animations. Awards section ok.

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-console.log("CONNECTED")
+console.log("App.js CONNECTED")
 
 const toggleVisibility = (e) => {
-    //console.log("TOGGLE")
     //console.log(e.target)
-    //console.log(e.target.closest("div").childNodes[3])
+    //console.log(e.target.closest("div").childNodes[1])
+    //console.log(e.target.closest("div").childNodes)
 
     const target = e.target.closest("div");     // Target the full div
-    const secondChild = target.childNodes[3].classList.toggle("visible");   // Node[3] = .answer
-    
-    //console.log(secondChild)
+    const toggleArrow = target.childNodes[1].classList.toggle("up");    // Node[1] = button
+    const answerVisibility = target.childNodes[3].classList.toggle("visible");   // Node[3] = .answer
 }
 
 // The following method display ALL the .answer
